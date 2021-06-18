5fd4f4064436904f2fbad348628b9ca4b56cac41
Chiro <sachinsinghroxx@gmail.com>
2021-06-18 12:33:35 +0530

    added best time to sell stocks

---
+++ C++/array/easy/bestTimeToSellStocks/main.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int profit = 0;
+        int buy = prices[0];
+        int sell = prices[0];
+
+        for (int i = 0; i < prices.size(); i++)
+        {
+            buy = min(prices[i], buy);
+
+            profit = max(profit, prices[i] - buy);
+        }
+
+        return profit;
+    }
+};
